a68832b9a5a0c225196c612f881142ab9ebf8693
Martin Grotz <martin.grotz@mathema.de>
2024-02-29 09:32:49 +0100

    start implementing generators

--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,26 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, img)
+import Html exposing (Html, button, div, img, text)
 import Html.Attributes exposing (src, style)
+import Html.Events exposing (onClick)
 import Json.Decode as D
 import VitePluginHelper
-import Html exposing (text)
-import Html exposing (button)
-import Html.Events exposing (onClick)
+import Random.List
+import List.Extra
+import Dict
 
 
 type alias Model =
     { allCards : List Card
-       , error : Maybe String
+    , cardsSplitForPack : Maybe CardsSplitForPack
+    , error : Maybe String
     }
 
 
 initialModel =
     { allCards = []
+    , cardsSplitForPack = Nothing
     , error = Nothing
     }
 
@@ -114,35 +117,89 @@ type Card
     | Item ItemCard
     | Failed
 
+type alias Booster = 
+    { commonSteel : Card
+    , commonRuby : Card
+    , commonAmber : Card
+    , commonSapphire : Card
+    , commonEmerald : Card
+    , commonAmethyst : Card
+    , uncommon1 : Card
+    , uncommon2 : Card
+    , uncommon3 : Card
+    , rare1: Card
+    , rare2: Card
+    , foil: Card
+    }
+    
 
 type Msg
     = NoOp
     | GeneratePacks
 
-type Rarity = Common | Uncommon | Rare | SuperRare | Legendary
-type Color = Steel | Ruby | Amber | Sapphire | Emerald | Amethyst 
+
+type Rarity
+    = Common
+    | Uncommon
+    | Rare
+    | SuperRare
+    | Legendary
+
+
+type Color
+    = Steel
+    | Ruby
+    | Amber
+    | Sapphire
+    | Emerald
+    | Amethyst
+
 
 decodeRarity : String -> D.Decoder Rarity
 decodeRarity rarityText =
     case rarityText of
-        "Common" -> D.succeed Common
-        "Uncommon" -> D.succeed Uncommon
-        "Rare" -> D.succeed Rare
-        "Super Rare" -> D.succeed SuperRare
-        "Legendary" -> D.succeed Legendary
-        _ -> D.fail ("Rarity '" ++ rarityText ++ "' could not be decoded")
+        "Common" ->
+            D.succeed Common
+
+        "Uncommon" ->
+            D.succeed Uncommon
+
+        "Rare" ->
+            D.succeed Rare
+
+        "Super Rare" ->
+            D.succeed SuperRare
+
+        "Legendary" ->
+            D.succeed Legendary
+
+        _ ->
+            D.fail ("Rarity '" ++ rarityText ++ "' could not be decoded")
 
 
 decodeColor : String -> D.Decoder Color
 decodeColor colorText =
     case colorText of
-        "Steel" -> D.succeed Steel
-        "Ruby" -> D.succeed Ruby
-        "Amber" -> D.succeed Amber
-        "Sapphire" -> D.succeed Sapphire
-        "Emerald" -> D.succeed Emerald
-        "Amethyst" -> D.succeed Amethyst
-        _ -> D.fail ("Color '" ++ colorText ++ "' could not be decoded")
+        "Steel" ->
+            D.succeed Steel
+
+        "Ruby" ->
+            D.succeed Ruby
+
+        "Amber" ->
+            D.succeed Amber
+
+        "Sapphire" ->
+            D.succeed Sapphire
+
+        "Emerald" ->
+            D.succeed Emerald
+
+        "Amethyst" ->
+            D.succeed Amethyst
+
+        _ ->
+            D.fail ("Color '" ++ colorText ++ "' could not be decoded")
 
 
 classifyAndDecodeCard : D.Decoder Card
@@ -184,7 +241,7 @@ decodeCharacterCard =
         (D.field "Strength" D.int)
         (D.field "Type" D.string)
         (D.field "Willpower" D.int)
-    |> D.map Character
+        |> D.map Character
 
 
 decodeSongCard : D.Decoder Card
@@ -215,7 +272,7 @@ decodeSongCard =
     D.map2 (<|)
         fieldSet1
         (D.field "Type" D.string)
-    |> D.map Song
+        |> D.map Song
 
 
 decodeActionCard : D.Decoder Card
@@ -246,7 +303,7 @@ decodeActionCard =
     D.map2 (<|)
         fieldSet1
         (D.field "Type" D.string)
-    |> D.map Action
+        |> D.map Action
 
 
 decodeItemCard : D.Decoder Card
@@ -277,7 +334,7 @@ decodeItemCard =
     D.map2 (<|)
         fieldSet1
         (D.field "Type" D.string)
-    |> D.map Item
+        |> D.map Item
 
 
 decodeCard : String -> D.Decoder Card
@@ -295,14 +352,13 @@ decodeCard cardType =
         "Item" ->
             decodeItemCard
 
-        _ -> D.fail ("cardType " ++ cardType ++ " failed to decode")
+        _ ->
+            D.fail ("cardType " ++ cardType ++ " failed to decode")
 
 
 main : Program D.Value Model Msg
 main =
     Browser.element { init = init, update = update, view = view, subscriptions = \_ -> Sub.none }
-
-
 
 
 init : D.Value -> ( Model, Cmd Msg )
@@ -315,11 +371,13 @@ init flags =
             case parsedCards of
                 Err e ->
                     { error = Just (D.errorToString e)
+                    , cardsSplitForPack = Nothing 
                     , allCards = []
                     }
 
                 Ok cards ->
                     { error = Nothing
+                    , cardsSplitForPack = Just (prepareCardLists cards)
                     , allCards = cards
                     }
     in
@@ -327,64 +385,116 @@ init flags =
     , Cmd.none
     )
 
-type alias CardsSplitForPack = 
-    {
-        commons: List Card
-        , uncommons: List Card
-        , rares: List Card
-        , superRares : List Card
-        , legendaries : List Card
-        , allCards: List Card
+
+type alias CardsSplitForPack =
+    { commons : List Card
+    , uncommons : List Card
+    , rares : List Card
+    , superRares : List Card
+    , legendaries : List Card
+    , allCards : List Card
     }
 
+
+colorFromCard : Card -> Color
+colorFromCard card =
+    case card of
+        Character x ->
+            x.color
+
+        Song x ->
+            x.color
+
+        Action x ->
+            x.color
+
+        Item x ->
+            x.color
+
+        Failed ->
+            Common
+
+rarityFromCard : Card -> Rarity
+rarityFromCard card =
+            case card of
+                Character x ->
+                    x.rarity
+
+                Song x ->
+                    x.rarity
+
+                Action x ->
+                    x.rarity
+
+                Item x ->
+                    x.rarity
+
+                Failed ->
+                    Common
 
 prepareCardLists : List Card -> CardsSplitForPack
 prepareCardLists allCards =
-    let 
-        rarityFromCard card = 
-            case card of
-                Character x -> x.rarity
-                Song x -> x.rarity
-                Action x -> x.rarity
-                Item x -> x.rarity
-                Failed -> Common
-    in
-    {
-        allCards = allCards
-        , commons = List.filter (\c -> rarityFromCard c == Common) allCards
-        , uncommons = List.filter (\c -> rarityFromCard c == Uncommon) allCards
-        , rares = List.filter (\c -> rarityFromCard c == Rare) allCards
-        , superRares = List.filter (\c -> rarityFromCard c == SuperRare) allCards
-        , legendaries = List.filter (\c -> rarityFromCard c == Legendary) allCards
+    { allCards = allCards
+    , commons = List.filter (\c -> rarityFromCard c == Common) allCards
+    , uncommons = List.filter (\c -> rarityFromCard c == Uncommon) allCards
+    , rares = List.filter (\c -> rarityFromCard c == Rare) allCards
+    , superRares = List.filter (\c -> rarityFromCard c == SuperRare) allCards
+    , legendaries = List.filter (\c -> rarityFromCard c == Legendary) allCards
     }
+
+
+generateCommon : Maybe (List Card) -> Random.Generator Card
+generateCommon possibleCommons =
+    case possibleCommons of
+            Nothing -> Failed
+            Just cards -> 
+
+generateBooster : CardsSplitForPack -> Random.Generator Booster
+generateBooster cardsSplitForPack =
+    let
+        groupedCards = List.Extra.gatherWith (\l r -> (colorFromCard l) == colorFromCard r) cardsSplitForPack.commons
+        colorsAsKeys = List.map (Tuple.mapFirst colorFromCard) groupedCards
+        asDict = Dict.fromList colorsAsKeys
+    in
+    
+    Random.map Booster (generateCommon (Dict.get Steel asDict))
+    |> andMap (generateCommon Ruby)
+    -- Random.List.shuffle cardsSplitForPack.commons |> gatherWith Color |> head of each group |> flatMap
+    -- Random.List.shuffle cardsSplitForPack.uncommons |> take 3
+    -- Random.List.shuffle cardsSplitForPack.rare |> take 2
+    -- Random.List.shuffle cardsSplitForPack.superRares |> take 2
+    -- Random.List.shuffle cardsSplitForPack.legendaries |> take 2
+    -- Random.List.shuffle cardsSplitForPack.allCards -- foil
+
 
 -- TODO: allCards -> prepareCardLists -> Shuffle Each List and "store" again + get three more random nums for the distributions
 -- -> use generated values into real generator function -> filter for pack set -> generate pack -> loop for packs -> store everything
 -- -> display everything
 -- 6 commons (**one of each color!**), 3 uncommons, 1 mit rare oder superrare oder legendary, 1 mit rare oder superrare oder legendary, 1 mit foil mit beliebiger rarity
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
+
         GeneratePacks ->
-            (model, Cmd.none)
-
-
-
+            ( model, Cmd.none )
 
 
 generatePacksSelection : Model -> Html Msg
 generatePacksSelection model =
-    button [onClick GeneratePacks] [text "Generate 3 TFC and 3 RotF packs"]
+    button [ onClick GeneratePacks ] [ text "Generate 3 TFC and 3 RotF packs" ]
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [
-            case model.error of
-                Nothing -> generatePacksSelection model
-                Just e -> div [] [text e]
-            
+        [ case model.error of
+            Nothing ->
+                generatePacksSelection model
+
+            Just e ->
+                div [] [ text e ]
         ]
